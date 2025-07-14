9f7891d31bdf0ca6d9efaebad7bb282994639591
DylanCarbone <94693787+DylanCarbone@users.noreply.github.com>
2025-07-14 15:26:51 +0100

    Update assessSpatialCov.R
    reverting some changes I made as they are not necessary

--- R/assessSpatialCov.R
+++ R/assessSpatialCov.R
@@ -1,186 +1,274 @@
 #' \code{assessSpatialCov}
 #'
-#' Grids and maps species occurrence data using specified CRS.
-#'
-#' @param dat A data.frame containing species occurrence data.
-#' @param species Column name in dat giving species names.
-#' @param x Column name in dat giving x coordinates.
-#' @param y Column name in dat giving y coordinates.
-#' @param year Column name in dat giving years.
-#' @param spatialUncertainty Column name in dat giving spatial uncertainty (optional).
-#' @param identifier Column name for grouping data.
-#' @param periods List of year sequences, e.g., list(1950:1990, 1991:2019).
-#' @param res Spatial resolution for gridding.
-#' @param logCount Whether to log-transform the raster values.
-#' @param countries ggplot2::map_data() country names or a list of them.
-#' @param shp Shapefile object (sp class), or list of them.
-#' @param maxSpatUncertainty Optional numeric cutoff for filtering uncertainty.
-#' @param output "density", "overlap", or "nPeriods".
-#' @param minPeriods Minimum number of periods required per cell (for "overlap").
-#' @param returnRaster Whether to return underlying rasters.
-#' @param crs Either "BNG" or "WGS84", specifying the CRS of the x/y input.
-#'
-#' @return A list of ggplot2 maps and optionally rasters.
+#' This function grids and then maps species occurrence data.
+#' @param dat string. A data.frame containing columns for species name, x coordinates, y coordinates, spatialUncertainty, year and an identifier (used to group the data - heuristic will be calculated for each group). 
+#' @param species Character string. column name in dat giving species names.
+#' @param x string. Column name in dat giving x coordinates. Any coordinate and spatial reference systems are permitted.
+#' @param y string. Column name in dat giving y coordinates. Any coordinate and spatial reference systems are permitted.
+#' @param year string. Column name in dat giving years.
+#' @param spatialUncertainty String. Column name in dat giving uncertainty associated with x and y. Any units are permitted. 
+#' @param identifier String. Column name in dat giving record "identifiers". Identifiers are used to group the data; heuristics will be calculated separately for each group.
+#' @param periods String. A list of time periods. For example, for two periods, the first spanning 1950 to 1990, and the second 1991 to 2019: periods = list(1950:1990, 1991:2019).
+#' @param res Numeric. Spatial resolution at which to grid the occurrence data.
+#' @param logCount Logical. Whether to log transform counts for visual purposes. Useful where there is large variation in counts across cells. 
+#' @param countries String or vector. Country names to be passed to ggplot2::map_data. Needed to add country borders to your plot. Countries should only be used if you are working on
+#'        the WGS84 coordinate reference system. Otherwise see shp. If the levels in the identifier field of dat are spatial units (e.g. countries), then the countries argument should be provided a list object. 
+#'        The names of each element in the list should correspond to one level of identifier, and the contents of that element should be a country name that is valid for use with ggplot2::map_data. Both countries and shp may 
+#'        be left unspecified, but in this case no country borders will be presented on the returned plots. Do not specify both countries and shp.
+#' @param shp String. If you are not working on WGS84, then you can provide a shapefile (spatialPolygons or spatialPolygonsDataFrame) with the country borders on the relevant crs for plotting. As with countries, 
+#'            if the levels in identifier denote spatial subsets of the data, then shp may be provided as a list, with the name of each element corresponding to one level of the identifier field in dat. 
+#'            Do not specify both countries and dat.
+#' @param maxSpatUncertainty Numeric. Maximum permitted spatial uncertainty. All records more uncertain than this value will be dropped. Units must match the units in your data.
+#' @param output String. Either "density", "overlap" or "nPeriods". If density then the maps show the density of records (grid cell^-1) per period and level of identifier. 
+#'               If overlap then one map is returned per level of identifier showing which cells have been sampled in >= \code{minPeriods} periods.
+#'               If nPeriods then one map is returned per level of identifier showing the number of periods in which each grid cell has been sampled.
+#' @param minPeriods Numeric. Lower limit of periods with sampling for cells to show up on the map. Defaults to NULL in which case only grid cells sampled in all \code{periods} are shown. This argument
+#'                   only applies if \code{output} = "overlap".
+#' @param returnRaster Logical. Whether or not to return rasters underpinning maps. This might be useful if you want to manipulate them in some way (e.g. convert the number of periods sampled to the proportion of periods samples, etc.).                    
+#' @return By default alist with n ggplot2 objects where n is the number of levels in the identifier field of dat. Where \code{output} = density return one map per level of identifier.
+#' @seealso \code{\link{assessSpatialBias}} which gives a measure of how far your data eviates from a random distribution in space. 
+#' @importFrom rasterVis gplot
 #' @export
+
 assessSpatialCov <- function(dat,
-                                      species,
-                                      x,
-                                      y,
-                                      year,
-                                      spatialUncertainty,
-                                      identifier,
-                                      res,
-                                      logCount = FALSE,
-                                      countries = NULL,
-                                      shp = NULL,
-                                      periods,
-                                      maxSpatUncertainty = NULL,
-                                      output = "density",
-                                      minPeriods = NULL,
-                                      returnRaster = FALSE,
-                                      crs = "BNG") {
+                             species,
+                             x,
+                             y,
+                             year,
+                             spatialUncertainty,
+                             identifier,
+                             res, 
+                             logCount = FALSE, 
+                             countries = NULL, 
+                             shp = NULL, 
+                             periods, 
+                             maxSpatUncertainty = NULL,
+                             output = "density",
+                             minPeriods = NULL,
+                             returnRaster = FALSE) {
+
+  if (any(!(c(species, x, y, year, spatialUncertainty, identifier) %in% colnames(dat)))) stop("You have specified columns that don't exist in dat.")
   
-  if (any(!(c(species, x, y, year, identifier) %in% colnames(dat)))) {
-    stop("You have specified columns that don't exist in dat.")
+  dat <- createData(data = dat,
+                    species,
+                    x,
+                    y,
+                    year,
+                    spatialUncertainty,
+                    identifier)
+  
+  if (is.list(countries) | is.list(shp)) print("You have specified shp or countries as a list object; this should only be the case if your identifier field denotes spatial subsets of your data.")
+  
+  if (!is.null(shp) & !is.null(countries)) stop("Only one of countries and shp may be specified; they do the same thing. If you are working on WGS84 it is recommended that you use countries; otherwise, you cannot use countries and should use shp.")
+  
+  if (any(is.na(dat$identifier))) stop("One or more NAs in the identifier field. NAs are not permitted.")
+  
+  if (!is.null(maxSpatUncertainty)) dat <- dat[!is.na(dat$spatialUncertainty) & dat$spatialUncertainty <= maxSpatUncertainty, ]
+
+  if (nrow(dat) == 0) stop("No records with with spatialUncertainty < maxSpatUncertainty")
+  
+  if (any(is.na(dat$year))) {
+    
+    warning("Removing data without a specified year")
+    
+    dat <- dat[-which(is.na(dat$year)), ]
+    
   }
   
-  if (!is.null(spatialUncertainty) && !(spatialUncertainty %in% colnames(dat))) {
-    stop("spatialUncertainty column specified but not found in data.")
+  if (any(!dat$year %in% unlist(periods))) {
+    
+    drop <- which(!dat$year %in% unlist(periods))
+    
+    dat <- dat[-drop, ]
+    
   }
 
-  # Optional filtering by spatial uncertainty
-  if (!is.null(spatialUncertainty) && !is.null(maxSpatUncertainty)) {
-    dat <- dat[!is.na(dat[[spatialUncertainty]]) & dat[[spatialUncertainty]] <= maxSpatUncertainty, ]
-    if (nrow(dat) == 0) stop("No records remain after applying spatial uncertainty filter.")
-  }
-
-  # Remove NA years
-  dat <- dat[!is.na(dat[[year]]), ]
-
-  # Filter for relevant time periods
-  all_years <- unlist(periods)
-  dat <- dat[dat[[year]] %in% all_years, ]
+  dat <- dat[order(dat$year), ]
+  
   dat$Period <- NA
-  for (i in seq_along(periods)) {
-    dat$Period[dat[[year]] %in% periods[[i]]] <- paste0("p", i)
+  
+  for (i in 1: length(periods)) {
+    
+    dat$Period <- ifelse(dat$year %in% periods[[i]], paste0("p", i), dat$Period)
+    
   }
 
-  # Validate CRS input
-  if (!(crs %in% c("BNG", "WGS84"))) stop("crs must be either 'BNG' or 'WGS84'")
-
-  sf_crs <- if (crs == "BNG") 27700 else 4326
-  dat$X_TEMP <- dat[[x]]
-  dat$Y_TEMP <- dat[[y]]
+  xmin <- min(dat$x, na.rm = T)
   
-  sf_dat <- sf::st_as_sf(dat, coords = c("X_TEMP", "Y_TEMP"), crs = sf_crs)
+  xmax <- max(dat$x, na.rm = T)
+  
+  ymin <- min(dat$y, na.rm = T)
+  
+  ymax <- max(dat$y, na.rm = T)
 
-  coords <- sf::st_coordinates(sf_dat)
-  dat$x <- coords[, 1]
-  dat$y <- coords[, 2]
+  rast <- raster::raster(ncol=length(seq(xmin, xmax, res)),
+                         nrow=length(seq(ymin, ymax, res)),
+                         xmn=xmin,
+                         xmx=xmax,
+                         ymn=ymin,
+                         ymx=ymax)
+  
+  for (i in unique(dat$identifier)) {
 
-  xmin <- min(dat$x, na.rm = TRUE)
-  xmax <- max(dat$x, na.rm = TRUE)
-  ymin <- min(dat$y, na.rm = TRUE)
-  ymax <- max(dat$y, na.rm = TRUE)
+    rasts <- lapply(X=unique(dat$Period), 
+                    function(x) { data <- dat[dat$identifier == i & dat$Period == x, c("x", "y")]
+                    
+                    if (nrow(data) >= 1) {
+                      
+                      raster::rasterize(data, rast)
+                      
+                    } else {
+                      
+                      r <- raster::setValues(rast, NA)
+                      
+                      r
+                      
+                    }
+                    
+                    })
+                    
 
-  rast <- raster::raster(ncol = length(seq(xmin, xmax, res)),
-                         nrow = length(seq(ymin, ymax, res)),
-                         xmn = xmin, xmx = xmax, ymn = ymin, ymx = ymax)
+    names(rasts) <- unique(dat$Period)
 
-  raster_list <- list()
-
-  for (id in unique(dat[[identifier]])) {
-    subset_dat <- dat[dat[[identifier]] == id, ]
-    rasts <- lapply(unique(subset_dat$Period), function(period) {
-      period_dat <- subset_dat[subset_dat$Period == period, ]
-      if (nrow(period_dat) > 0) {
-        raster::rasterize(period_dat[, c("x", "y")], rast)
-      } else {
-        raster::setValues(rast, NA)
-      }
-    })
-
-    names(rasts) <- unique(subset_dat$Period)
     rasts <- raster::stack(rasts)
 
-    if (logCount) rasts <- log10(rasts)
+    if (logCount == TRUE) rasts <- log10(rasts)
 
     if (output == "density") {
-      raster_list[[id]] <- rasts
+      
+      assign(paste0("rasts", i), rasts)
+      
     } else if (output == "nPeriods") {
-      summed <- sum(as.logical(rasts), na.rm = TRUE)
-      summed[summed == 0] <- NA
-      raster_list[[id]] <- summed
-    } else if (output == "overlap") {
-      if (is.null(minPeriods)) minPeriods <- length(unique(subset_dat$Period))
-      summed <- sum(as.logical(rasts), na.rm = TRUE)
-      summed[summed < minPeriods] <- NA
-      raster_list[[id]] <- as.logical(summed)
-    }
-  }
-
-  myCol <- rgb(255, 255, 255, max = 255, alpha = 125)
-  legend_title <- switch(output,
-                         "density" = ifelse(logCount, "log10(n records)", "n records"),
-                         "nPeriods" = "Number of periods sampled",
-                         "overlap" = "Sampled in minPeriods periods")
-
-  plots <- lapply(names(raster_list), function(id) {
-    rast <- raster_list[[id]]
-
-    if (!is.null(countries) || !is.null(shp)) {
-      if (!is.null(shp)) {
-        map <- if (is.list(shp)) ggplot2::fortify(shp[[id]]) else ggplot2::fortify(shp)
-      } else {
-        map_data <- ggplot2::map_data("world", regions = if (is.list(countries)) countries[[id]] else countries)
-        map <- map_data
-      }
+      
+      rasts <- sum(as.logical(rasts), na.rm = T)
+      
+      rasts[rasts == 0] <- NA
+      
+      assign(paste0("rasts", i), rasts)
+      
     } else {
-      map <- NULL
+
+      if (is.null(minPeriods)) minPeriods <- length(unique(dat$Period))
+
+      rasts <- sum(as.logical(rasts), na.rm = T)
+
+      rasts[rasts < minPeriods] <- NA
+      
+      assign(paste0("rasts", i), as.logical(rasts))
+      
     }
-
-    p <- rasterVis::gplot(rast) + ggplot2::theme_linedraw()
-
-if (output == "density") {
-  p <- p + ggplot2::geom_tile(ggplot2::aes(fill = value))
-
-  # Only facet if there is more than one unique period (i.e., raster layer)
-  if (raster::nlayers(rast) > 1) {
-    p <- p + ggplot2::facet_wrap(~ variable)
+    
   }
 
-  p <- p + ggplot2::scale_fill_gradient2(
-    low = "red", high = "blue", na.value = myCol,
-    name = legend_title
-  )
-} else {
-      n <- length(unique(raster::getValues(rast)))
-      p <- p +
-        ggplot2::geom_tile(ggplot2::aes(fill = factor(value))) +
-        ggplot2::scale_fill_manual(values = terrain.colors(n, rev = TRUE),
-                                   na.value = myCol,
-                                   name = legend_title,
-                                   na.translate = FALSE) +
-        ggplot2::ggtitle(id)
-      if (output == "overlap") {
-        p <- p + ggplot2::theme(legend.position = "none")
-      }
-    }
-
-    if (!is.null(map)) {
-      p <- p + ggplot2::geom_polygon(data = map,
-                                     ggplot2::aes(x = long, y = lat, group = group),
-                                     colour = "black", fill = myCol, inherit.aes = FALSE)
-    }
-
-    p
-  })
-
-  names(plots) <- names(raster_list)
-
-  if (returnRaster) {
-    return(list(plots = plots, rasters = raster_list))
+  if (output == "overlap") {
+    
+    leg <- "sampled in minPeriods
+    periods"
+    
+  } else if (output == "density") {
+    
+    leg <- ifelse(logCount == TRUE, "log10(n records)", "n records")
+    
   } else {
-    return(plots)
+    
+    leg <- "Number of periods sampled"
+    
   }
+  
+  
+  myCol <- rgb(255,255,255, max = 255, alpha = 125, names = "blue50")
+
+  ## add lapply to return multiple plots as a list
+
+  out <- lapply(as.character(unique(dat$identifier)),
+                function(x) {
+                  
+                  if (is.list(countries) & !any(!countries %in% unique(ggplot2::map_data("world")$region)) |
+                      !is.list(countries) & !is.null(countries) & !any(!countries %in% unique(ggplot2::map_data("world")$region)) |
+                      !is.null(shp)) {
+                    
+                    if (is.null(shp)) { 
+                      
+                      if (is.list(countries)) {
+                        
+                        map <- ggplot2::map_data("world", regions = countries[[x]])
+                        
+                      } else {
+                        
+                        map <- ggplot2::map_data("world", regions = countries)
+                        
+                      }
+                      
+                    } else { 
+                      
+                      if (is.list(shp)) {
+                        
+                        map <- ggplot2::fortify(shp[[x]])
+                        
+                      } else {
+                        
+                        map <- ggplot2::fortify(shp)
+                        
+                      }
+                      
+                    }
+                    
+                  } else {
+                    
+                    warning("Some or all country names provided are not in unique(ggplot2::map_data(world)$region)")
+                    
+                    map <- NULL
+                    
+                  }
+
+
+                  p <- rasterVis::gplot(get(paste0("rasts", x))) +
+                    ggplot2::theme_linedraw()
+                  
+                  if (output == "density") {
+                    
+                    p <- p + ggplot2::geom_tile(ggplot2::aes(fill = value)) +
+                      ggplot2::facet_wrap(~ variable) + 
+                      ggplot2::scale_fill_gradient2(low = "red", high = "blue", na.value = myCol,
+                                                    name = leg)
+                    
+                  } else {
+                    
+                    n <- length(unique(raster::getValues(get(paste0("rasts", x)))))
+                    
+                    p <- p + ggplot2::geom_tile(ggplot2::aes(fill = factor(value))) +
+                      ggplot2::scale_fill_manual(values = terrain.colors(n, rev = TRUE), 
+                                                 na.value = myCol, name = leg,
+                                                 na.translate = FALSE) +
+                      ggplot2::ggtitle(x)
+                  
+                    if (output == "overlap") p <- p + ggplot2::theme(legend.position = "none") 
+                    
+                  }
+
+                  if (!is.null(map)) p <- p + ggplot2::geom_polygon(data = map, ggplot2::aes(x=long, y = lat, group = group),
+                                                                                 colour="black",fill=myCol,
+                                                                                 inherit.aes=F)
+                  return(p)
+                  
+                  })
+  
+  names(out) <- unique(dat$identifier)
+  
+  if(returnRaster) {
+    
+    r <- raster::stack(lapply(unique(dat$identifier),
+                              function(x) {
+                                get(paste0("rasts", x))
+                              }))
+    
+    names(r) <- unique(dat$identifier)
+    
+    out <- list(plots = out,
+                rasters = r)
+    
+  }
+
+return(out)
+
 }
